dd2af82f85a3aebf827054154ad203f2d1594118
김지웅 <godjw96@gmail.com>
2021-11-11 04:23:10 +0900

    rest

---
+++ src/test/java/com/example/resttodo/ResttodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resttodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
