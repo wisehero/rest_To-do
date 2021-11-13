7b2486dd81bcccf96ebffd52dfd212d72fbe59bc
김지웅 <godjw96@gmail.com>
2021-11-13 19:12:52 +0900

    Update TodoEntity.java

--- src/main/java/com/example/resttodo/model/TodoEntity.java
+++ src/main/java/com/example/resttodo/model/TodoEntity.java
@@ -4,12 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "Todo")
 public class TodoEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id;
     private String userId;
     private String title; // to-do 타이틀 (ex: 코테준비)
