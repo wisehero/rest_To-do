be01d221161d4470069685fa294229de2c07ba3d
김지웅 <godjw96@gmail.com>
2021-11-13 19:12:46 +0900

    Create TodoRepository.java

---
+++ src/main/java/com/example/resttodo/persistence/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.example.resttodo.persistence;
+
+import com.example.resttodo.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    List<TodoEntity> findByUserId(String userId);
+}
