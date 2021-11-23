e2d393f193aaf2b611a1b34190746ea9ec9f7d0b
김지웅 <godjw96@gmail.com>
2021-11-23 22:19:43 +0900

    Update JwtAuthenticationFilter.java

--- src/main/java/com/example/resttodo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/resttodo/security/JwtAuthenticationFilter.java
@@ -33,7 +33,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             String token = parseBearerToken(request);
             log.info("Filter is running....");
             // 토큰 검사하기  JWT이므로 인가 서버에 요청하지 않고도 검증 가능
-            if (token != null && token.equalsIgnoreCase("null")) {
+            if (token != null && !token.equalsIgnoreCase("null")) {
                 // userId 가져오기. 위조된 경우 예외 처리 한다.
                 String userId = tokenProvider.validateAndGetUserId(token);
                 log.info("Authenticated user ID : " + userId);
