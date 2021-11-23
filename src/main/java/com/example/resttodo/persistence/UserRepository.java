8c22cbb47689ff2a769eb921dfb2cc89561b2ea9
김지웅 <godjw96@gmail.com>
2021-11-23 21:30:26 +0900

    Create UserRepository.java

---
+++ src/main/java/com/example/resttodo/persistence/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.resttodo.persistence;
+
+import com.example.resttodo.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    UserEntity findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+    UserEntity findByEmailAndPassword(String email, String password);
+}
