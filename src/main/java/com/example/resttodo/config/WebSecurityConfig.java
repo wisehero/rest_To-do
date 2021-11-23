4ccabc090fa371461266e9d7dce41eec8481596d
김지웅 <godjw96@gmail.com>
2021-11-23 22:05:56 +0900

    Create WebSecurityConfig.java

---
+++ src/main/java/com/example/resttodo/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.resttodo.config;
+
+import com.example.resttodo.security.JwtAuthenticationFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Slf4j
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // http 시큐리티 빌더
+        http.cors() // 기본 cors tjfwjd
+                .and()
+                .csrf().disable()
+                .httpBasic().disable() // BASIC 인증 비활성화
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 기반이 아님을 선언
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/auth/**").permitAll() // /와 /auth/** 경로는 인증 필요 X
+                .anyRequest().authenticated(); // /와 /auth/** 이외의 모든 경로는 인증해야 됨
+        // filter  등록
+        // 매 요청마다
+        // CorsFilter 실행한 후에
+        // jwtAuthenticationFilter 실행한다.
+        http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+    }
+}
